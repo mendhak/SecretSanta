10a436194ff81d4cf917cd49bb2e53ae8fede4b7
Gavin Chin <slowmonkeyking+github@gmail.com>
2015-11-27 00:28:01 +0000

    Removing commented out code

--- app/mainPage/mainController.js
+++ app/mainPage/mainController.js
@@ -1,13 +1,5 @@
 "use strict";
 
-// angular.module('ssMainPage').controller('MainController', ['$scope',
-//     function($scope) {
-//         $scope.registerNewOrganiser = function() {
-//             alert('hello');
-//         }
-//     }
-// ]);
-
 var mainController = function() {
     this.registerNewOrganiser = function() {
         console.log("registerNewOrganiser" + new Date());
